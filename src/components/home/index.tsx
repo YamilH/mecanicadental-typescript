83269767b2e0c6af047429b5f6485977b359f6b2
YamilH <yamilhamui@gmail.com>
2023-11-08 19:12:13 -0300

    sponsor beta

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,9 +3,9 @@ import { SelectedPage } from "@/shared/types";
 import ActionButton from "@/shared/ActionButton";
 import HomePageText from "@/assets/HomePageText.png";
 import HomePageGraphic from "@/assets/HomePageGraphic.png";
-import SponsorRedBull from "@/assets/SponsorRedBull.png";
-import SponsorForbes from "@/assets/SponsorForbes.png";
-import SponsorFortune from "@/assets/SponsorFortune.png";
+import SponsorColtene from "@/assets/SponsorColtene.png";
+import Sponsor3M from "@/assets/Sponsor3M.png";
+import SponsorIvoclar from "@/assets/SponsorIvoclar.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { motion } from "framer-motion";
 
@@ -65,8 +65,8 @@ const Home = ({ setSelectedPage }: Props) => {
             </ActionButton>
             <AnchorLink
               className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
-              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
-              href={`#${SelectedPage.ContactUs}`}
+              onClick={() => setSelectedPage(SelectedPage.Contact)}
+              href={`#${SelectedPage.Contact}`}
             >
               <p>Learn More</p>
             </AnchorLink>
@@ -85,11 +85,11 @@ const Home = ({ setSelectedPage }: Props) => {
       {/* SPONSORS */}
       {isAboveMediumScreens && (
         <div className="h-[150px] w-full bg-primary-100 py-10">
-          <div className="mx-auto w-5/6">
+          <div className="mx-auto w-4/6">
             <div className="flex w-3/5 items-center justify-between gap-8">
-              <img alt="redbull-sponsor" src={SponsorRedBull} />
-              <img alt="forbes-sponsor" src={SponsorForbes} />
-              <img alt="fortune-sponsor" src={SponsorFortune} />
+              <img alt="coltene-sponsor" src={SponsorColtene} />
+              <img alt="3m-sponsor" src={Sponsor3M} />
+              <img alt="ivoclar-sponsor" src={SponsorIvoclar} />
             </div>
           </div>
         </div>
