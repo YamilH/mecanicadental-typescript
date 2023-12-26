bc81b63a1a4aada7e1f926ef3e3044a1a6bdd4f1
YamilH <yamilhamui@gmail.com>
2023-12-26 19:26:07 -0300

    fix color in the footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <>
       
 
-      <footer className="bg-white dark:bg-gray-100">
+      <footer className="bg-primary-100">
     <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
